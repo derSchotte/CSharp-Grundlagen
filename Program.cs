3d9caddc27238c86a405e41eef1732c42023ff54
Frank McDonald <kontakt@frank-mcdonald.de>
2022-10-05 16:45:31 +0200

    add BubbleSort Programm

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine( "Geben Sie eines der folgenden Programm Namen ein!" );
 Console.WriteLine( "[1]HelloWorld, [2]FizzBuzz, [3]AtoO, [4]BinToDez, [5]BinToDez2, [6]Noten, " +
     "\n[7]ReverseInput, [8]Taschenrechner, [9]Vererbung, [10]PerlinNoise, [11]<List>Noten2, [12]HackNSlay, " +
-    "\n[13]Password, [14]Check Email, [15]NewPassword, [16]LottoZahlen" );
+    "\n[13]Password, [14]Check Email, [15]NewPassword, [16]LottoZahlen, [17]BubbleSort" );
 Console.Write( "Eingabe Zahl: " );
 string input = Console.ReadLine();
 Console.Clear();
@@ -56,6 +56,9 @@ switch( input ) {
     break;
     case "16":
     LottoGenerator.runLottoGenerator();
+    break;
+    case "17":
+    BubbleSort.runBubbleSort();
     break;
     default:
     Console.WriteLine( "Sie haben nicht das eingegeben was gebraucht wird!" );
---
+++ kleineProgramme/BubbleSort.cs
@@ -0,0 +1,58 @@
+﻿namespace Grundlagen.kleineProgramme {
+    internal class BubbleSort {
+        public static void runBubbleSort() {
+            int[] mixedArray = {23,5,7,12,45,13,88,67,34,234};
+            int temp = 0;
+            int index = 0;
+
+            foreach( int p in mixedArray ) {
+                Console.Write( $"[{p}], " );
+            }
+            Console.WriteLine( "\n" );
+
+            for( int j = 0; j <= mixedArray.Length - 2; j++ ) {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine( $"Durchgang J: '{j}'" );
+
+                for( int i = 0; i < mixedArray.Length - 2; i++ ) {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine( $"Durchgang I: '{i}'" );
+                    
+                    if( mixedArray[ i ] > mixedArray[ i + 1 ] ) {
+                        temp = mixedArray[ i + 1 ];
+                        
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine( $"Lege Index {i + 1} in Temp: {mixedArray[ i + 1 ]}" );
+                        
+                        mixedArray[ i + 1 ] = mixedArray[ i ];
+                        
+                        Console.WriteLine( $"Lege Index {i} in Index {i + 1}: {mixedArray[ i ]}" );
+                        
+                        mixedArray[ i ] = temp;
+                        
+                        Console.WriteLine( $"Lege Temp in index {i}: {temp}" );
+                        Console.ResetColor();
+
+                        int c = 0;
+                        foreach( int p in mixedArray ) {
+                            Console.Write( $"{c++}[{p}], " );
+                        }
+                    } else {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    
+                        Console.WriteLine( $"Index {i} ist nicht größer als {i + 1}" );
+                        Console.ResetColor();
+                    }
+                    
+                    Console.Write( "\n\n" );
+                }
+            }
+
+            Console.WriteLine( "Bubble sort array:" );
+
+            foreach( int p in mixedArray ) {
+                Console.Write( p + " " );
+            }
+        }
+    }
+}
